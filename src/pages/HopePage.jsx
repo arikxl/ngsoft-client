c8412f0c7397fdf66130369bdb8ecf0046d153a6
Arik Alexandrov <arikxl@gmail.com>
2022-05-22 14:56:31 +0300

    init client

---
+++ src/pages/HopePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AppHeader from '../components/AppHeader'
+
+const HopePage = () => {
+  return (
+    <div>
+        <AppHeader />
+    </div>
+  )
+}
+
+export default HopePage
